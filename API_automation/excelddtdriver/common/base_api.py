97d6f8a37d44346c4c37d7628804034a62fcd6c2
hello-T <123@163.com>
2018-06-29 09:50:25 +0800

    Automation1.0

---
+++ API_automation/excelddtdriver/common/base_api.py
@@ -0,0 +1,116 @@
+# coding:utf-8
+import json
+import requests
+from excelddtdriver.common.readexcel import ExcelUtil
+from excelddtdriver.common.writeexcel import copy_excel, Write_excel
+import base64
+import hashlib
+
+def curlmd5(a):
+    m = hashlib.md5()
+    m.update(a.encode('UTF-8'))
+    return m.hexdigest()
+
+
+def send_requests(s, testdata):
+    '''封装requests请求'''
+    method = testdata["method"]
+    url = testdata["url"]
+    # url后面的params参数
+    try:
+        params = eval(testdata["params"])
+    except:
+        params = None
+    # 请求头部headers
+    try:
+        headers = eval(testdata["headers"])
+        print("请求头部：%s" % headers)
+    except:
+        headers = None
+    # post请求body类型
+    type = testdata["type"]
+
+    test_nub = testdata['id']
+    print("*******正在执行用例：-----  %s  ----**********" % test_nub)
+    print("请求方式：%s, 请求url:%s" % (method, url))
+    print("请求params：%s" % params)
+
+    # post请求body内容
+    try:
+        bodydata = eval(testdata["body"])
+    except:
+        bodydata = {}
+
+    #判断此接口是否是加密请求
+    try:
+        encrypt = testdata["encrypt"]
+    except:
+        encrypt=None
+
+    #判断data是否需要加密
+    if encrypt==0:
+        bodydata_T=bodydata
+    else:
+        bodydata_encrypt=base64.b64encode(str(curlmd5(json.dumps(bodydata) + curlmd5('OWNjZGY0MmNmOE1U'))[0:10]+str(base64.b64encode(json.dumps(bodydata).encode('utf-8')),'utf-8')).encode('utf-8')).decode()
+        bodydata_T=bodydata_encrypt
+
+
+    # 判断传data数据还是json
+    if type == "data":
+        body = bodydata_T
+    elif type == "json":
+        body = json.dumps(bodydata_T)
+    else:
+        body = bodydata_T
+    if method == "post": print("post请求body类型为：%s ,body内容为：%s" % (type, body))
+
+    verify = False
+    res = {}   # 接受返回数据
+
+    try:
+        r = s.request(method=method,
+                      url=url,
+                      params=params,
+                      headers=headers,
+                      data=body,
+                      verify=verify
+                       )
+        print("页面返回信息：%s" % r.content.decode("utf-8"))
+        res['id'] = testdata['id']
+        res['rowNum'] = testdata['rowNum']
+        res["statuscode"] = str(r.status_code)  # 状态码转成str
+        res["text"] = r.content.decode("utf-8")
+        res["times"] = str(r.elapsed.total_seconds())   # 接口请求时间转str
+        if res["statuscode"] != "200":
+            res["error"] = res["text"]
+        else:
+            res["error"] = ""
+        res["msg"] = ""
+        if testdata["checkpoint"] in res["text"]:
+            res["result"] = "pass"
+            print("用例测试结果:   %s---->%s" % (test_nub, res["result"]))
+        else:
+            res["result"] = "fail"
+        return res
+    except Exception as msg:
+        res["msg"] = str(msg)
+        return res
+
+def wirte_result(result, filename="result.xlsx"):
+    # 返回结果的行数row_nub
+    row_nub = result['rowNum']
+    # 写入statuscode
+    wt = Write_excel(filename)
+    wt.write(row_nub, 8, result['statuscode'])       # 写入返回状态码statuscode,第8列
+    wt.write(row_nub, 9, result['times'])            # 耗时
+    wt.write(row_nub, 10, result['error'])            # 状态码非200时的返回信息
+    wt.write(row_nub, 12, result['result'])           # 测试结果 pass 还是fail
+    wt.write(row_nub, 13, result['msg'])           # 抛异常
+
+if __name__ == "__main__":
+    data = ExcelUtil("debug_api.xlsx").dict_data()
+    print(data[0])
+    s = requests.session()
+    res = send_requests(s, data[0])
+    copy_excel("debug_api.xlsx", "result.xlsx")
+    wirte_result(res, filename="result.xlsx")
---
+++ API_automation/excelddtdriver/run_all.py
@@ -0,0 +1,41 @@
+# coding=utf-8
+import unittest
+import time
+from excelddtdriver.common import HTMLTestRunner_jpg
+import os
+import datetime
+
+
+
+curpath = os.path.dirname(os.path.realpath(__file__))
+report_path = os.path.join(curpath, "report")
+if not os.path.exists(report_path): os.mkdir(report_path)
+case_path = os.path.join(curpath, "case")
+
+def add_case(casepath=case_path, rule="test*.py"):
+    '''加载所有的测试用例'''
+    # 定义discover方法的参数
+    discover = unittest.defaultTestLoader.discover(casepath,
+                                                  pattern=rule,)
+
+    return discover
+
+def run_case(all_case, reportpath=report_path):
+    '''执行所有的用例, 并把结果写入测试报告'''
+    # 生成当前时间戳
+    nowtime = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    htmlreport = reportpath+r"\result"+nowtime+".html"
+    print("测试报告生成地址：%s"% htmlreport)
+    fp = open(htmlreport, "wb")
+    runner = HTMLTestRunner_jpg.HTMLTestRunner(stream=fp,
+                                               verbosity=2,
+                                               title="测试报告",
+                                               description="用例执行情况")
+
+    # 调用add_case函数返回值
+    runner.run(all_case)
+    fp.close()
+
+if __name__ == "__main__":
+    cases = add_case()
+    run_case(cases)
